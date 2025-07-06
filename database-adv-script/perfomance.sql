a690f1f4a5e3fb50b2459c8011a215c56d31974e
Namsophia <namsophie9@gmail.com>
2025-07-06 23:18:08 +0200

    edited performance file

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -6,7 +6,7 @@ INNER JOIN Property ON b.property_id = p.property_id
 LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
 WHERE b.start_date >= CURRENT_DATE - INTERVAL '90 days' AND b.status = 'confirmed';
 
-2. Analysing query performance
+2. EXPLAIN query performance
 
 SELECT b.booking_id, b.start_date, b.end_date, b.status, u.user_id, u.first_name, u.last_name, u.email, p.property_id, p.name, p.location, pay.payment_id, pay.amount, pay.payment_method
 FROM Booking 
