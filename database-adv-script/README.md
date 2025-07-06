ab0015a1a7d1a5d22970c88a81984e5e5087663e
Namsophia <namsophie9@gmail.com>
2025-07-06 19:42:12 +0200

    created queries with joins

---
+++ database-adv-script/README.md
@@ -0,0 +1,4 @@
+Queries created 
+1. INNER JOIN - retrieves all bookings and their  respective users who made the bookings
+2. LEFT JOIN - retrieves all properties and their reviews
+3. FULL OUTER JOIN - retrieves all users and all bookings even if the user has no booking.
---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,14 @@
+INNER JOIN
+SELECT * b.booking_id, b.start_date, b.end_date, b.total_price, b.status, u.user_id, u.first_name, u.last_name, u.email
+FROM Bookings INNER JOIN Users
+ON b.user_id = u.user_id;
+
+LEFT JOIN
+SELECT * p.property_id, p.name, p.location, r.review_id, r.rating, r.comment
+FROM Property LEFT JOIN Review
+ON p.property_id = r.property_id;
+
+FULL OUTER JOIN
+SELECT * u.user_id, u.first_name, u.last_name, b.booking_id, b.start_date, b.end_date, b.total_price
+FROM FULL User OUTER JOIN Booking
+ON u.user_id = b.user_id;
